08b8a1f82cd27792c62389e979408774c5417629
Felix Vicente <felixvicent1306@gmail.com>
2021-12-30 14:51:10 -0300

    Create Project

---
+++ android/app/src/main/kotlin/com/example/gestapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gestapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(GestApp());
+}
+
+class GestApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
