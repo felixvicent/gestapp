30eeb2da34b6ef68b19e0e2c98c06c1f43acb0fa
Felix Vicente <felixvicent1306@gmail.com>
2022-01-01 10:45:53 -0300

    Style chartbar

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -19,15 +19,35 @@ class ChartBar extends StatelessWidget {
       children: [
         Text(
           'R\$${value.toStringAsFixed(2)}',
-          style: TextStyle(
-            fontSize: 13,
-          ),
         ),
         SizedBox(height: 5),
         Container(
           height: 60,
           width: 10,
-          child: null,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
         SizedBox(height: 5),
         Text(label),
